58e92ae465e498bd42219f9a71e008c980fb48af
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 18:02:33 -0300

    se crea el position

--- models/sensorModel.js
+++ models/sensorModel.js
@@ -4,10 +4,21 @@ const Schema = mongoose.Schema
 const SensorSchema = Schema({
   id: {type: String, required: true},
   name: {type: String, required: true},
-  latitude: {type: String, required: true},
-  length: {type: String, required: true},
-  last_connection: {type: Date, required: false},
-  last_alert: {type: Date, required: false}
+  position: {
+    lat: {type: Number, required: true},
+    lng: {type: Number, required: true}
+  },
+  last_connection: {type: Date, required: false, default: Date.now},
+  last_alert: {type: Date, required: false, default: Date.now}
+},
+{
+  toObject: {virtuals: true, getters: true},
+  toJSON: {virtuals: true, getters: true} 
+})
+
+SensorSchema.virtual('date_last_connection').get(function() {
+  let fecha = new Date(this.last_connection)
+  return fecha.getDate
 })
 
 export const sensorModel = mongoose.model('Sensor', SensorSchema)
