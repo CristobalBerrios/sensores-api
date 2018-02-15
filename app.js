9d89447b73b665accf7a70650e6ea8bd11e7e716
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 18:00:32 -0300

    se cargan las apiRoutes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import sensorRoutes from './routes/sensorRoutes'
+import apiRoutes from './routes/apiRoutes'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extend: false}))
 app.use(bodyParser.json())
 app.use(cors())
-app.use('/api', sensorRoutes)
+app.use('/api', apiRoutes)
 
 export default app
