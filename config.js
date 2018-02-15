b96741d6071bf632afec00743129631437e919d8
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 20:10:00 -0300

    babel configurations

--- config.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-  PORT: process.env.PORT || 3001,
-  DB: process.env.MONGODB || 'mongodb://localhost:27017/',
-  SECRET: 'jwtsecret'
-}
--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const Schema = mongoose.Schema
-
-const UserSchema = Schema({
-  email: {type: String, required: true},
-  password: {type: String, required: true}
-})
-
-export const userModel = mongoose.model('User', UserSchema)
