6831cfa4e5daccb67d1e6f661828de8fd5b42a01
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 20:42:31 -0300

    mongolab url set

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 export const config = {
   PORT: process.env.PORT || 3001,
-  DB: process.env.MONGODB_URL || 'mongodb://xiters:123@ds119598.mlab.com:19598/',
+  DB: process.env.MONGOLAB_URI || 'mongodb://xiters:123@ds119598.mlab.com:19598/',
   SECRET: 'jwtsecret'
 }
