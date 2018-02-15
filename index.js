684dc7c94dd3980c9f7095cc60ff4dfdfeb0b3aa
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 18:00:55 -0300

    se cargan variables del archivo config

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import app from './app'
+import {config} from './config'
 import mongoose from 'mongoose'
-const port = process.env.PORT || 3001
 
-mongoose.connect('mongodb://localhost:27017/sismos', (err, res) => {
+mongoose.connect(config.DB +'sismos', (err, res) => {
 	if (err) return console.log('Error al conectarse a mongodb')
 
 	console.log('Conexion con mongodb exitosa')
 
-	app.listen(port, () => {
-		console.log('Aplicacion corriendo en el puerto ' + port)
+	app.listen(config.PORT, () => {
+		console.log('Aplicacion corriendo en el puerto ' + config.PORT)
 	})
 })
