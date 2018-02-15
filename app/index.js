e4d5ae212c45cfdc0df45c7c93f4cff0d283637c
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 20:33:19 -0300

    console.log para ver ruta de la base de datos

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import app from './app'
 import {config} from './config'
 import mongoose from 'mongoose'
 
+console.log('la base de datos esta en:' + config.DB)
+
 mongoose.connect(config.DB +'sismos', (err, res) => {
 	if (err) return console.log('Error al conectarse a mongodb' + config.DB)
 
