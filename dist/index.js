850c73ee708c763540533162659898e4e493c276
Cristobal Berrios <elcriss96@gmail.com>
2018-02-15 20:35:58 -0300

    new production files

--- dist/index.js
+++ dist/index.js
@@ -12,8 +12,10 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+console.log('la base de datos esta en:' + _config.config.DB);
+
 _mongoose2.default.connect(_config.config.DB + 'sismos', function (err, res) {
-	if (err) return console.log('Error al conectarse a mongodb');
+	if (err) return console.log('Error al conectarse a mongodb' + _config.config.DB);
 
 	console.log('Conexion con mongodb exitosa');
 
