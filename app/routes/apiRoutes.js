cad6f564748cb7051f5c338b26c90a059b27af71
Cristobal Berrios <elcriss96@gmail.com>
2018-02-25 19:31:16 -0300

    se agregan las rutas para los eventos

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import auth from '../middlewares/auth'
 import {userController} from '../controllers/UserController'
 import {sensorController} from '../controllers/SensorController'
+import {eventController} from '../controllers/EventController'
 
 const route = express.Router()
 
@@ -15,6 +16,11 @@ route.get('/sensor', sensorController.getAll)
 route.get('/sensor/:id', sensorController.getById)
 route.post('/sensor', sensorController.save)
 route.put('/sensor/:id', sensorController.update)
-route.delete('sensor/:id', sensorController.remove)
+route.delete('/sensor/:id', sensorController.remove)
+
+// Event Routes
+route.use('/event', auth.isAuth)
+route.get('/event', eventController.getAll)
+route.post('/event', eventController.save)
 
 export default route
