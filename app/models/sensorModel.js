9ce70e6f291fb4f3d13b408e5866d24075ae6270
Cristobal Berrios <elcriss96@gmail.com>
2018-02-28 00:02:12 -0300

    se agrega columna cluster

--- app/models/sensorModel.js
+++ app/models/sensorModel.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const SensorSchema = Schema({
-  id: {type: String, required: true},
+  id: {type: String, required: true, unique: true},
   name: {type: String, required: true},
   position: {
     lat: {type: Number, required: true},
     lng: {type: Number, required: true}
   },
-  last_connection: {type: Date, required: false, default: Date.now},
-  last_alert: {type: Date, required: false, default: Date.now}
+  last_connection: {type: Date, required: false},
+  cluster: {type: Schema.ObjectId, ref: 'Cluster', required: true}
 })
 
 export const sensorModel = mongoose.model('Sensor', SensorSchema)
