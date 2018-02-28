c9880c65976e6d2142faef5600ba3f3d02fe2f0a
Cristobal Berrios <elcriss96@gmail.com>
2018-02-28 00:01:42 -0300

    se cambia arreglo de sensors por arreglo de clusters

--- app/models/eventModel.js
+++ app/models/eventModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const EventSchema = Schema({
   date: {type: Date, default: Date.now},
   intensity: {type: Number, required: true},
-  sensors: [{type: Schema.ObjectId, ref: 'Sensor', required: true}]  
+  clusters: [{type: Schema.ObjectId, ref: 'Cluster', required: true}]  
 })
 
 export const eventModel = mongoose.model('Event', EventSchema)
