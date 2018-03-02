c7f37537822348cf03a03bc25d52a99a8cf7b6df
Cristobal Berrios <elcriss96@gmail.com>
2018-03-02 12:30:29 -0300

    se agrega virtual

--- app/models/sensorModel.js
+++ app/models/sensorModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import moment from 'moment'
 const Schema = mongoose.Schema
 
 const SensorSchema = Schema({
@@ -9,7 +10,21 @@ const SensorSchema = Schema({
     lng: {type: Number, required: true}
   },
   last_connection: {type: Date, required: false},
+  last_event: {type: Date, required: false},
   cluster: {type: Schema.ObjectId, ref: 'Cluster', required: true}
+},
+{
+  toJSON: {virtuals: true}
+}
+)
+
+SensorSchema.virtual('isOnline').get(function () {
+  if (this.last_connection == null || this.last_connection == '') return false
+  let last_connection = moment(this.last_connection)
+  let now = moment()
+
+  if ((now - last_connection) > 90000) return false
+  return true
 })
 
 export const sensorModel = mongoose.model('Sensor', SensorSchema)
