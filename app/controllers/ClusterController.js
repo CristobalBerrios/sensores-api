6f24fde38ae75286e0ff0ac71fd832b3344a9c40
Cristobal Berrios <elcriss96@gmail.com>
2018-03-02 12:26:00 -0300

    se agrega el remove y update

--- app/controllers/ClusterController.js
+++ app/controllers/ClusterController.js
@@ -14,7 +14,28 @@ class ClusterController {
 
     cluster.save((err, newCluster) => {
       if (err) return res.status(500).send({message: 'Error al guardar el cluster'})
-      return res.status(200).send({newCluster})
+      return res.status(200).send(newCluster)
+    })
+  }
+
+  update (req, res) {
+    const clusterId = req.params.id
+    const clusterUpdate = req.body
+  
+    clusterModel.findByIdAndUpdate(clusterId, clusterUpdate, (err, cluster) => {
+      if (err) return res.status(500).send({message: 'error al editar el cluster'})
+      if(!cluster) return res.status(404).send({message: 'cluster no encontrado al editar'})
+      res.status(200).send({cluster: cluster})
+    })
+  }
+
+  remove (req, res) {
+    const clusterId = req.params.id
+
+    clusterModel.findByIdAndRemove(clusterId, (err, cluster) => {
+      if (err) return res.status(500).send({message: 'error al eliminar el cluster'})
+      if(!cluster) return res.status(404).send({message: 'cluster no encontrado al eliminar'})
+      res.status(200).send({message: 'cluster eliminado', id: cluster._id})
     })
   }
 }
